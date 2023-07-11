9dbcf5a8a37d2ca758be75294e7acdda24111330
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-07-11 23:50:47 +0300

    g

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { UnsplashAPI } from '../src/axios';
 import Notiflix from 'notiflix';
-
+import { lightbox } from '../src/simplelightbox';
 const form = document.querySelector('.search-form');
 const galery = document.querySelector('.galery-list');
 const load = document.querySelector('.load-more');
@@ -12,22 +12,47 @@ async function handleFormSubmit(e) {
   const info = e.currentTarget.elements['searchQuery'].value.trim();
   request.query = info;
   request.page = 1;
+  galery.innerHTML = '';
+
   try {
-    const date = await request.fetchPhotos();
-    const { hits, total } = date;
-    if (!hits.length) {
-      console.log(
+    const dates = await request.fetchPhotos();
+    const { data } = dates;
+    if (!data.hits.length) {
+      load.style.display = 'none';
+      Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.!'
       );
       throw new Error();
     }
-    createGallery(hits);
+    if (info === '') {
+      Notiflix.Notify.info('Ведіть дані пошуку');
+    }
+
+    createGallery(data.hits);
     load.style.display = 'block';
   } catch (err) {
     console.log(err);
   }
 }
-async function handleClick() {}
+const handleClick = async () => {
+  request.page += 1;
+
+  try {
+    const { data } = await request.fetchPhotos();
+    isShown = data.hits.length;
+    if (isShown === data.totalHits) {
+      load.style.display = 'none';
+      Notiflix.Notify.info(
+        ' Were sorry, but youve reached the end of search results'
+      );
+    }
+
+    galery.insertAdjacentHTML('beforeend', createGallery(data.hits));
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
 form.addEventListener('submit', handleFormSubmit);
 load.addEventListener('click', handleClick);
 function createGallery(arry) {
@@ -69,4 +94,5 @@ function createGallery(arry) {
     )
     .join('');
   galery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
