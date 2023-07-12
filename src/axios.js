7bcf8c8c05b18490cdcebf357f9a636742e811de
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-07-12 16:22:21 +0300

    gh

--- src/axios.js
+++ src/axios.js
@@ -5,14 +5,14 @@ export class UnsplashAPI {
 
   page = 1;
   query = null;
-
+  per_page = 40;
   async fetchPhotos() {
     return await axios.get(`${this.#BASE_URL}/`, {
       params: {
         q: this.query,
         key: '38174659-9b0c55b9f6b140ee944fe291a',
         page: this.page,
-        per_page: 40,
+        per_page: this.per_page,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
